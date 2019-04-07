60d4828b5e66dab31719f94d9989de53b784665c
Tomas Gallucci <professortom@gmail.com>
2019-04-07 10:43:24 -0500

    add a WindowController class

---
+++ Grid Browser/WindowController.swift
@@ -0,0 +1,21 @@
+//
+//  WindowController.swift
+//  Grid Browser
+//
+//  Created by Tomas Gallucci on 4/7/19.
+//  Copyright © 2019 Blue Portal Coding. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        window?.titleVisibility = .hidden
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
