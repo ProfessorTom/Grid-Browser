d3ee87c2ec575a6924973c3549038ca5eb024b54
Tomas Gallucci <professortom@gmail.com>
2019-04-07 10:05:24 -0500

    Initial Commit

---
+++ Grid Browser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Grid Browser
+//
+//  Created by Tomas Gallucci on 4/7/19.
+//  Copyright © 2019 Blue Portal Coding. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Grid Browser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Grid Browser
+//
+//  Created by Tomas Gallucci on 4/7/19.
+//  Copyright © 2019 Blue Portal Coding. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
