7753db1314e500f6d05cca5d37901f88aa0ee6b0
Tomas Gallucci <professortom@gmail.com>
2019-04-07 10:51:15 -0500

    remove comment

--- Grid Browser/WindowController.swift
+++ Grid Browser/WindowController.swift
@@ -14,8 +14,6 @@ class WindowController: NSWindowController {
         super.windowDidLoad()
         
         window?.titleVisibility = .hidden
-    
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
 
 }
